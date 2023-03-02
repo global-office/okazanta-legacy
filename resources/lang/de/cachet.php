31889e6c635a3af8a1a2ba808e585cb23df6818c
Patric Steffen <patric.steffen@global-office.de>
2023-03-02 16:24:33 +0100

    Updated some translations

--- resources/lang/de/cachet.php
+++ resources/lang/de/cachet.php
@@ -55,9 +55,9 @@ return [
 
     // Service Status
     'service' => [
-        'good'  => '[0,1]System funktioniert|[2,*]Alle Systeme funktionieren',
-        'bad'   => '[0,1] Das System hat momentan Probleme|[2,*] Mehrere Systeme haben momentan Probleme',
-        'major' => '[0,1] Das System hat ein schwerwiegendes Problem|[2,*] Mehrere Systeme haben ein schwerwiegendes Problem',
+        'good'  => '[0,1]System funktioniert|[2,*]Alle Systeme funktionieren.',
+        'bad'   => '[0,1]Das System hat momentan Probleme!|[2,*]Mehrere Systeme haben momentan Probleme!',
+        'major' => '[0,1]Das System hat ein schwerwiegendes Problem!|[2,*]Mehrere Systeme haben ein schwerwiegendes Problem!',
     ],
 
     'api' => [
